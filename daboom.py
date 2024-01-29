255b836a29744cbf398bba60889e051a6a7b1f77
Adul23 <147740606+Adul23@users.noreply.github.com>
2024-01-29 21:28:41 +0600

    Update daboom.py

--- daboom.py
+++ daboom.py
@@ -7,9 +7,9 @@ from sympy import *
 # input intervals from terminal or in code
 # a = int(input())
 # b = int(input())
-a = 2
+a = -5
 a1 = a
-b = 4
+b = 5
 print(f"input intervals [{a}, {b}]")
 # input N from terminal or in code
 # n = int(input())
@@ -22,17 +22,20 @@ print(f"{dx} is delta")
 print("input function")
 # input expression from terminal or in code
 # s = input()
-s = "log(x) * cos(x) * pi *pi"
+s = "(x) ** 2 + 5"
 print(f"{s}")
 func = []
 x_val = np.linspace(a1, b)
 y_val = [eval(s.replace('x', str(val))) for val in x_val]
+
 plt.plot(x_val, y_val)
 sum1 = 0
 while a <= b:
     w = s.replace("x", str(a))
     func.append(eval(w))
+    print(a)
     a += dx
+print(func)
 for i, e in enumerate(func):
     if i == 0 or i == len(func) - 1:
         sum1 += e
@@ -69,7 +72,6 @@ for i in range(n):
 
     x_trap = [a1 + i * dx, a1 + (i + 1) * dx, a1 + (i + 1) * dx, a1 + i * dx, a1 + i * dx]
     y_trap = [0, 0, eval(s.replace("x", str(a1 + (i + 1) * dx))), eval(s.replace("x", str(a1 + i * dx))), 0]
-    print(x_trap, y_trap)
     plt.plot(x_trap, y_trap, 'r--')
 plt.legend()
 plt.title("Function and Trapezoids")
@@ -79,30 +81,35 @@ plt.show()
 # print som
 print(f"By Simpson's Rule S = {S}")
 m = diff(y, *2 * [x['x']])  # m that is in E
-# plt.show()
 res2 = []
-for j in range(a1, b + 1):
+a = a1
+while (a1 <= b):
     k = ""
     for i in str(m):
         if i == 'x':
-            k += str(j)
+            k += str(a1)
         else:
             k += i
     res2.append(eval(k))
+    print((k))
+    a1 += dx
 m = (max(res2))
 Et = m * (b - a1) ** 3 / (12 * n ** 2)
-# print(m)
 print(f"Error estimation is {Et}")
+
 m2 = diff(y, *4 * [x['x']])
+print(m2)
 res1 = []
-for j in range(a1, b + 1):
+while a <= b:
     k = ""
     for i in str(m2):
         if i == 'x':
-            k += str(j)
+            k += str(a)
         else:
             k += i
     res1.append(eval(k))
+    a += dx
 m2 = (max(res1))
+print(m, m2)
 Es = m2 * ((b - a1) ** 5) / (180 * (n ** 4))
 print(f"Error estimation of Simpson's law is {Es}")
