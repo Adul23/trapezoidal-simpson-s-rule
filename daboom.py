63644eb09b4130c915ae0a3c3983f58586fbd301
Adul23 <147740606+Adul23@users.noreply.github.com>
2024-02-01 18:09:24 +0600

    Update daboom.py

--- daboom.py
+++ daboom.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import *
+
 init_printing(use_unicode=False, wrap_line=False)
 # input intervals from terminal or in code
 # a = int(input())
 # b = int(input())
-a = -1
+a = 1
 a1 = a
-b = 10
+b = 4
 jab = a
 print(f"input intervals [{a}, {b}]")
 # input N from terminal or in code
 # n = int(input())
-n = 13
+n = 20
 
 print(f"input N = {n}")
 # find delta x
@@ -21,7 +22,8 @@ print(f"{dx} is delta")
 print("input function")
 # input expression from terminal or in code
 # s = input()
-s = "x * e"
+s = "sin(x * pi)"
+why = s
 s = s.replace('sin', 'np.sin')
 # s = s.replace('e', 'np.e')
 s = s.replace('cos', 'np.cos')
@@ -33,14 +35,10 @@ s = s.replace('pi', 'np.pi')
 
 print(f"{s}")
 func = []
-x_val = np.linspace(a1, b)
-y_val = [eval(s.replace('x', str(val))) for val in x_val]
-plt.plot(x_val, y_val)
-plt.savefig('da')
+
 sum1 = 0
 while a <= b:
     w = s.replace("x", str(a))
-    print(w)
     try:
         result = eval(w)
         func.append(eval(w))
@@ -69,27 +67,36 @@ for i, e in enumerate(func):
         sum2 += 2 * eval(str(e))
         d = False
 
-# print Simpson
 S = (dx / 3) * sum2
+# print Simpson's quantity
+print(f"By Simpson's Rule S = {S}")
 x = {'x': Symbol('x', real=True)}
 q = s.replace('np.', '')
 y = parse_expr(q, x)
-print(y)
-# making a graphic
 s = s.replace('np.sin', 'sin')
 s = s.replace('np.pi', 'pi')
-x_val = np.linspace(a1, b, 100)
-y_val = [eval(s.replace("x", str(val))) for val in x_val]
-# Plotting trapezoids
-# Plotting the function
-plt.plot(x_val, y_val, label='Function')
+s = s.replace('np.cos', 'cos')
+s = s.replace('np.tan', 'tan')
+
+# making graphic
+# plotting
+x_val = np.linspace(a1, b)
+y_val = []
+for val in x_val:
+    w = s.replace("x", str(a))
+    try:
+        y_val.append(eval(w))
+    except ZeroDivisionError:
+        print("Error: Division by zero")
+
+plt.plot(x_val, y_val, label=f'{why}', color='black')
 
 # Plotting trapezoids
 for i in range(n):
     x_trap = [a1 + i * dx, a1 + (i + 1) * dx, a1 + (i + 1) * dx, a1 + i * dx, a1 + i * dx]
     try:
         y_trap = [0, 0, eval(s.replace("x", str(a1 + (i + 1) * dx))), eval(s.replace("x", str(a1 + i * dx))), 0]
-        plt.plot(x_trap, y_trap, 'r-', alpha=0.5)  # Use 'r-' for solid red lines
+        plt.plot(x_trap, y_trap, 'r-', alpha=0.5, color='red')  # Use 'r-' for solid red lines
     except ZeroDivisionError:
         print("Error: Division by zero")
 
@@ -99,10 +106,8 @@ plt.xlabel("x")
 plt.ylabel("y")
 
 plt.show()
-
-# print som
-print(f"By Simpson's Rule S = {S}")
-m = diff(y, *2 * [x['x']])  # m that is in E
+# Error T
+m = diff(y, *2 * [x['x']])  # second derivative
 res2 = []
 a = a1
 while a1 <= b:
@@ -121,7 +126,8 @@ m = (max(res2))
 Et = m * (b - a1) ** 3 / (12 * n ** 2)
 print(f"Error estimation is {Et}")
 
-m2 = diff(y, *4 * [x['x']])
+m2 = diff(y, *4 * [x['x']])  # 4 derivative
+
 res1 = []
 while a <= b:
     k = ""
@@ -131,20 +137,15 @@ while a <= b:
         else:
             k += i
     try:
-
         res1.append(eval(k))
     except ZeroDivisionError:
         print("div by 0")
     a += dx
 m2 = (max(res1))
+print(res1)
 Es = m2 * ((b - a1) ** 5) / (180 * (n ** 4))
-print(y)
 c1 = integrate(str(y), Symbol('x'))
 string1 = str(c1)
-print(string1)
-
-string12 = string1.replace('cos', f'np.cos')
-string2 = string1.replace('cos', f'np.cos')
 string12 = string1.replace('x', f'{jab}')
 string2 = string1.replace('x', f'{b}')
 print(f"Error estimation of Simpson's law is {Es}")
